abb057319ac6669cad12a6689a0007830e1f8c5f
hanchchch <cndghks15@gmail.com>
2023-03-31 14:49:48 +0900

    fix: user-data

--- resource/user-data.sh
+++ resource/user-data.sh
@@ -15,10 +15,10 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 chmod a+r /etc/apt/keyrings/docker.gpg
-apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-useradd -ms /bin/bash runner
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+useradd -ms /bin/bash ubuntu
 groupadd docker
-usermod -aG docker runner
+usermod -aG docker ubuntu
 
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -32,13 +32,13 @@ curl -o actions-runner-linux-x64-$ACTIONS_RUNNER_VERSION.tar.gz -L https://githu
 tar xzf ./actions-runner-linux-x64-$ACTIONS_RUNNER_VERSION.tar.gz
 ./bin/installdependencies.sh
 
-cat <<- EOF > $RUN_SCRIPT
-newgrp docker
-./config.sh --url "$GITHUB_URL" --pat "$GITHUB_TOKEN" --name $PUBLIC_IP --runnergroup default --work _work --labels self-hosted,Linux,X64 > config.log
-nohup ./runsvc.sh > action.log &
-EOF
-
 chown -R ubuntu:ubuntu /home/ubuntu
 
-su ubuntu -c $RUN_SCRIPT
+sudo -u ubuntu -i <<- EOF
+newgrp docker
+cd /home/ubuntu
+./config.sh --url "$GITHUB_URL" --pat "$GITHUB_TOKEN" --name $PUBLIC_IP --runnergroup default --work _work --labels self-hosted,Linux,X64 > config.log
+nohup ./run.sh > action.log &
+EOF
+
 
